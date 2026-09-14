ac079251fa0098330bb197d406d5748805e383e3
Álvaro Holguera <correo@laultimapregunta.com>
2026-09-14 16:19:41 +0200

    Redondear con dos decimales y concatenar texto con variable

--- DEIM-T/Assets/_MyAssets/01_Scripts/PlayerManager.cs
+++ DEIM-T/Assets/_MyAssets/01_Scripts/PlayerManager.cs
@@ -2,15 +2,19 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    float timeElapsed;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        print("Hola Mundo");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timeElapsed = Time.time;
+        float timeRounded = Mathf.Round((timeElapsed * 100)) / 100;
+        print("Tiempo transcurrido: " + timeRounded);
     }
 }
